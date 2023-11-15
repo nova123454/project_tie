7f1ccec6ff627b2dd286da336c25e82efdaf9cc6
nova123454 <139088342+nova123454@users.noreply.github.com>
2023-11-15 16:32:14 +0900

    2023-11-15

---
+++ js/tie.js
@@ -0,0 +1,27 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  grabCursor: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+$(".pj_img").hover(
+  function () {
+    let img_h = $(this).find("img").height();
+    let screen_h = $(window).height();
+
+    $(this)
+      .find("img")
+      .stop()
+      .animate({ "margin-top": -(img_h - screen_h) }, 4000);
+  },
+  function () {
+    $(this).find("img").stop().animate({ "margin-top": 0 }, 2000);
+  }
+);
